c6921a7845776fbeb4f514be4a65ae45daa7e0ac
sbastiaens <62792658+sbastiaens@users.noreply.github.com>
2021-11-12 10:03:38 -0500

    Update n170.py

--- eegnb/experiments/visual_n170/n170.py
+++ eegnb/experiments/visual_n170/n170.py
@@ -6,15 +6,25 @@ from optparse import OptionParser
 
 import numpy as np
 from pandas import DataFrame
-from psychopy import visual, core, event
-
+from psychopy import visual, core, event, prefs, logging
+import h5py
+import socket
+import json
 from eegnb import generate_save_fn
 from eegnb.stimuli import FACE_HOUSE
 
 __title__ = "Visual N170"
 
+prefs.resetPrefs()
+prefs.hardware['audioDriver'] = ["portaudio"]
+prefs.hardware['audioLib'] = ['PTB', 'pyo','pygame']
 
-def present(duration=120, eeg=None, save_fn=None):
+def sendpack(data_to_send):
+    event_to_send = json.dumps(data_to_send).encode("utf-8")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(event_to_send, ("239.128.35.86", 7891))
+    
+def present(duration=120, eeg=None, kernel=None, save_fn=None):
     n_trials = 2010
     iti = 0.4
     soa = 0.3
@@ -48,10 +58,22 @@ def present(duration=120, eeg=None, save_fn=None):
                 f"No path for a save file was passed to the experiment. Saving data to {save_fn}"
             )
         eeg.start(save_fn, duration=record_duration + 5)
-
+    
+     if kernel:
+        timestamp = start*1e9
+        timestamp = int(timestamp)
+        data_to_send = {
+        "id": 0,
+        "timestamp": timestamp,
+        "event": "start_experiment",
+        "value": "0",
+        }
+        sendpack(data_to_send)
+        evlen=0
+        
     # Start EEG Stream, wait for signal to settle, and then pull timestamp for start point
     start = time()
-
+    
     # Iterate through the events
     for ii, trial in trials.iterrows():
         # Inter trial interval
@@ -70,7 +92,23 @@ def present(duration=120, eeg=None, save_fn=None):
             else:
                 marker = markernames[label]
             eeg.push_sample(marker=marker, timestamp=timestamp)
-
+        
+        if kernel:
+            if trial['image_type']==0:
+              dev_v_st = "houses"
+            if trial['image_type']==1:
+              dev_v_st = "faces"
+            timestamp = time()*1e9
+            timestamp = int(timestamp)
+            data_to_send = {
+            "id": ii+1,
+            "timestamp": timestamp,
+            "event": dev_v_st,
+            "value":"1",
+            }
+            sendpack(data_to_send)
+            evlen += 1            
+            
         mywin.flip()
 
         # offset
@@ -86,7 +124,17 @@ def present(duration=120, eeg=None, save_fn=None):
         eeg.stop()
 
     mywin.close()
-
+    
+    if kernel:
+        timestamp = time()*1e9
+        timestamp = int(timestamp)
+        data_to_send = {
+        "id": evlen+1,
+        "timestamp": timestamp,
+        "event": "end_experiment",
+        "value": "2",
+        }
+        sendpack(data_to_send)
 
 def show_instructions(duration):
 
